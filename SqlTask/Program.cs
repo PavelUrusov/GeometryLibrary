7f1430457b190c50c92e5665c93834aab82ff679
null <pavell.urusov@gmail.com>
2023-11-11 01:16:40 +0300

    SqlTask

---
+++ SqlTask/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using Npgsql;
+
+Console.OutputEncoding = Encoding.UTF8;
+const string connectionString = "Host=localhost;Username=postgres;Password=admin;Database=psqltaskdb";
+
+using var connection = new NpgsqlConnection(connectionString);
+connection.Open();
+
+var psqlQuery = @"
+        SELECT
+            p.product_name AS ProductName,
+            c.category_name AS CategoryName
+        FROM
+            products p
+        LEFT JOIN
+            product_categories pc ON p.product_id = pc.product_id
+        LEFT JOIN
+            categories c ON pc.category_id = c.category_id;";
+
+using var command = new NpgsqlCommand(psqlQuery, connection);
+using var reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+    var productName = reader["ProductName"].ToString();
+    var categoryName = reader["CategoryName"].ToString();
+
+    Console.WriteLine($"ProductName: {productName}, CategoryName: {categoryName}");
+}
+
+return;
+
+
+void CreateProductCategoryTables(NpgsqlConnection connection)
+{
+    var createTables = @"
+            CREATE TABLE products (
+                product_id SERIAL PRIMARY KEY,
+                product_name VARCHAR(255) NOT NULL
+            );
+
+            CREATE TABLE categories (
+               category_id SERIAL PRIMARY KEY,
+               category_name VARCHAR(255) NOT NULL
+            );
+
+            CREATE TABLE product_categories (
+                product_id INT REFERENCES products(product_id),
+                category_id INT REFERENCES categories(category_id),
+                PRIMARY KEY (product_id, category_id)
+           );";
+
+    using (var command = new NpgsqlCommand(createTables, connection))
+    {
+        command.ExecuteNonQuery();
+    }
+
+    Console.WriteLine("Tables have been created successfully!");
+}
